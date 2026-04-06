d446837b8ec6e279c0a93b86f0c66b70ac46b0c8
PARV <arhatiaparv789@gmail.com>
2026-04-06 12:05:24 +0530

    Wire up efficient component architecture for main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,3 +7,12 @@ import './styles/global.css'
 
 const queryClient = new QueryClient();
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
