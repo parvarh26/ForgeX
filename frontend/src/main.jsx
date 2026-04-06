e1dc3aba592784b264479baee0a02d130a01a8ab
PARV <arhatiaparv789@gmail.com>
2026-04-06 11:27:37 +0530

    Optimize scalable navigational flows for main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './styles/global.css'
+
+const queryClient = new QueryClient();
+
